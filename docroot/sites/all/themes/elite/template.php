13eb5e59962f5dc46e42d7c715fe493a1f4d4481
alex <alex@ninthlink.com>
2013-10-28 16:35:40 -0700

    style based on if training quiz is taken

--- docroot/sites/all/themes/elite/template.php
+++ docroot/sites/all/themes/elite/template.php
@@ -10,3 +10,23 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+ 
+function elite_preprocess_views_view_responsive_grid(&$vars) {
+	if ( isset( $vars['rows'] ) ) {
+		$viewresults = $vars['view']->result;
+		$count = 0;
+		foreach ( $vars['rows'] as $i => $row ) {
+			foreach ( $row as $j => $col ) {
+				if ( isset( $viewresults[$count]->node_field_data_field_training_quiz_nid ) ) {
+					if ( $viewresults[$count]->node_field_data_field_training_quiz_nid == 1 ) {
+						// set in elite_custom.module, elite_custom_views_pre_render function
+						$vars['rows'][$i][$j]['classes'] .= ' taken';
+						$vars['rows'][$i][$j]['content'] = str_replace( 'views-field-field-base-points">', 'views-field-field-base-points"><span class="chk"></span>', $vars['rows'][$i][$j]['content'] );
+					}
+				}
+				$count++;
+			}
+		}
+//		watchdog('rowscheck', 'rows : <pre>'. print_r($vars['rows'],true) .'</pre><br />view results <pre>'. print_r($vars['view']->result,true) .'</pre>');
+	}
+}
