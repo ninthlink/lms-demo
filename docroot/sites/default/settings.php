9d4a17c32b0638ddd76d209f5d04564c1a79c23b
Troy Shields <troy@troyshields.com>
2013-11-07 14:00:07 -0500

    2450707 updating better messages styles and correct answers inline

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -2,8 +2,8 @@
 /**
  * This settings.php file was created by the Acquia Cloud ah-site-archive-import
  * Drush command. The imported archive did not contain a settings.php file, so
- * the import process created this file by default. You can replace this file 
- * with the standard default settings.php file for your version of Drupal. 
+ * the import process created this file by default. You can replace this file
+ * with the standard default settings.php file for your version of Drupal.
  * However, be sure to keep the last line that loads the "Acquia Cloud settings
  * include file", which provides the correct database credentials for your site.
  */
@@ -26,23 +26,23 @@ ini_set('url_rewriter.tags',        '');
 
 
 // On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this 
+// database in each site environment (Dev, Stage, or Prod). To use this
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/elite/elite-settings.inc');
 } else {
-	$databases = array();
+  $databases = array();
 
-	$databases['default']['default'] = array(
-		'driver' => 'mysql',
-		'database' => 'qelite',
-		'username' => 'root',
-		'password' => '',
-		'host' => 'localhost',
-		'prefix' => '',
-	);
-	//define('ELITELOCAL', 'http://192.168.0.57/elite/docroot');
+  $databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => 'elite',
+    'username' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'prefix' => '',
+  );
+  //define('ELITELOCAL', 'http://192.168.0.57/elite/docroot');
 }
 // t() overrides?
 $conf['locale_custom_strings_en'][''] = array(
