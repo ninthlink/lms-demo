4fcc0e4050887db2ecb22d12f2e9605a4863cb52
alex chousmith <alex@ninthlink.com>
2014-01-31 12:39:09 -0800

    initial es add

---
+++ docroot/meet-snapdragon/es.php
@@ -0,0 +1,449 @@
+<?php
+/*
+ * Qualcomm HTML5 Modules
+ * Meet Qualcomm® Snapdragon™ Processors
+ *
+ * v0.4.3
+ */
+?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Meet Snapdragon</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+
+		<link rel="stylesheet" href="css/main.css">
+
+		<script src="../wtrainings/_common/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><?php /* load CDN or local backup */ ?>
+		<script>window.jQuery || document.write('<script src="../wtrainings/_common/js/jquery-1.10.1.min.js"><\/script>')</script>
+
+		<script src="../wtrainings/_common/js/spin.min.js"></script>
+		<script src="../wtrainings/_common/js/jquery.scrollorama.js"></script>
+		<script src="../wtrainings/_common/js/waypoints.min.js"></script>
+		<script src="../wtrainings/_common/js/swiper.min.js"></script>
+
+	</head>
+
+	<body>
+		
+		<div id="wrapper">
+
+			<?php // Slide #1 ?>
+			<header class="bg-red-grad">
+				<div class="inner">
+					<h1 class="center txtw">Conozca los procesadores <span class="thick">Snapdragon</span><sup>&trade;</sup> de Qualcomm<sup>&reg;</sup></h1>
+					<div class="header-graphic">
+						<img id="header-device" src="img/header-phone.png" />
+						<img id="gear1" src="img/gear1.png" />
+						<img id="gear2" src="img/gear2.png" />
+					</div>
+				</div>
+			</header>
+			
+			<div id="content">
+				
+				<?php // Slide #2 ?>
+				<section id="loading" class="bg-white">
+					<div class="inner">
+						<div class="graphic-position">
+							<div id="spinner"></div>
+							<div id="spinner-inner" class="txtr">?</div>
+						</div>
+						<div class="text-position">
+							<h3 class="txtb">¿Alguna vez ha tenido que esperar a que se cargue una aplicación o un sitio web en su smartphone?</h3>
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #3 ?>
+				<section id="difference" class="bg-grey">
+					<div class="inner">
+						<div class="text-position">
+							<h2 class="txtr">Aprenda cómo los dispositivos Snapdragon de Qualcomm <span class="thick">pueden marcar la diferencia</span></h2>
+							<ul class="txtb">
+								<li id="parallax1">Qué es Qualcomm</li>
+								<li id="parallax2">La función del procesador en un smartphone</li>
+								<li id="parallax3">Por qué los dispositivos Snapdragon ofrecen una experiencia móvil extraordinaria</li>
+							</ul>
+						</div>
+						<div class="graphic-position">
+							<img id="chip1" src="img/chip218x213.png" />
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #4 ?>
+				<section id="earth" class="bg-black">
+					<div class="inner">
+						<div class="text-position">
+							<h2 class="txtw">Usted está aquí. <span class="thick">En la Tierra.</span></h2>
+							<p class="txtg">¡Qualcomm suministra <span class="thick">22 chips MSM cada segundo!</span> Desde que comenzamos, hemos enviado más de <span class="thick">15 mil millones de chips</span>. Lo que serían 2 chips por cada persona que habita el <span class="thick">planeta Tierra.</span></p>
+						</div>
+						<div class="graphic-position">
+							<div class="earth"><img src="img/earth.png" /></div>
+							<div class="ring wrapper4">
+							  <div class="pie spinner"></div>
+							  <div class="pie filler"></div>
+							  <div class="mask"></div>
+							</div>
+							<div class="ring wrapper3">
+							  <div class="pie spinner"></div>
+							  <div class="pie filler"></div>
+							  <div class="mask"></div>
+							</div>
+							<div class="ring wrapper2">
+							  <div class="pie spinner"></div>
+							  <div class="pie filler"></div>
+							  <div class="mask"></div>
+							</div>
+							<div class="ring wrapper1">
+							  <div class="pie spinner"></div>
+							  <div class="pie filler"></div>
+							  <div class="mask"></div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #5 ?>
+				<section id="every-phone" class="bg-grey-grad">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtb"><span class="thick">Cada smartphone</span> lleva un procesador</h2>
+							<p class="txtb">asegúrese de escoger un procesador con las funciones adecuadas</p>
+						</div>
+						<div class="graphic-position center multi-col">
+							<div class="scrollx">
+								<div id="speed_performance" class="three-row">
+									<div class="sprite"></div>
+									<p class="thick">Velocidad y rendimiento</p>
+								</div>
+								<div id="reliable_connectivity" class="three-row">
+									<div class="sprite"></div>
+									<p class="thick">Conectividad fiable</p>
+								</div>
+								<div id="long_battery_life" class="three-row last">
+									<div class="sprite"></div>
+									<p class="thick">Batería de larga duración</p>
+								</div>
+							</div>
+							<span class="stretch"></span>
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #6 ?>
+				<section id="processes" class="bg-red-grad-lt">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtw">La gente espera que los smartphones hagan <span class="thick">de todo</span>. Los procesadores Snapdragon son el núcleo de estos procesos</h2>
+						</div>
+						<div class="graphic-position center txtw multi-col">
+							<div id="process_text" class="four-row">
+								<div class="sprite"></div>
+								<p>texto</p>
+							</div>
+							<div id="process_movies" class="four-row">
+								<div class="sprite"></div>
+								<p>películas</p>
+							</div>
+							<div id="process_maps" class="four-row">
+								<div class="sprite"></div>
+								<p>mapas</p>
+							</div>
+							<div id="process_internet" class="four-row">
+								<div class="sprite"></div>
+								<p>internet</p>
+							</div>
+							<div id="process_apps" class="four-row">
+								<div class="sprite"></div>
+								<p>aplicaciones</p>
+							</div>
+							<div id="process_social" class="four-row">
+								<div class="sprite"></div>
+								<p>redes sociales</p>
+							</div>
+							<div id="process_photos" class="four-row">
+								<div class="sprite"></div>
+								<p>fotos</p>
+							</div>
+							<div id="process_videos" class="four-row">
+								<div class="sprite"></div>
+								<p>videos</p>
+							</div>
+							<div id="process_news" class="four-row">
+								<div class="sprite"></div>
+								<p>juegos</p>
+							</div>
+							<div id="process_shopping" class="four-row">
+								<div class="sprite"></div>
+								<p>compras</p>
+							</div>
+							<div id="process_emails" class="four-row">
+								<div class="sprite"></div>
+								<p>correos electrónicos y calendarios</p>
+							</div>
+							<div id="process_voice" class="four-row">
+								<div class="sprite"></div>
+								<p>voz</p>
+							</div>
+
+							<div class="four-row box-blind"></div>
+							<div class="four-row box-blind"></div>
+							<div class="four-row box-blind"></div>
+							<div class="four-row box-blind"></div>
+
+							<span class="stretch"></span>
+
+						</div>
+						<div id="phone-slide">
+							<img id="phone-with-chip" src="img/phone-with-chip.png" />
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #7 ?>
+				<section id="delivers" class="bg-grey-rdl">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtr">Snapdragon <span class="thick">ofrece</span></h2>
+						</div>
+						<div class="graphic-position center">
+							<div class="left">
+								<img id="chip2" src="img/chip223x228.png" />
+								<img id="chip-lines" src="img/chip-lines.png" />
+							</div>
+							<div class="right">
+								<ul>
+									<li id="delivers1">4G LTE</li>
+									<li id="delivers2">Conexiones más rápidas</li>
+									<li id="delivers3">Multimedia y gráficos</li>
+									<li id="delivers4">Las mejores experiencias</li>
+									<li id="delivers5">Menor tamaño, más ligero y moderno</li>
+									<li id="delivers6">Mayor duración de la batería</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #8 ?>
+				<section id="mobile-power">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtw"><span class="thick">Potencia móvil</span> extraordinaria</h2>
+							<h5 class="txtw">Los procesadores Snapdragon están equipados con un chip de una inmensa potencia móvil en un sistema diminuto.</h5>
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #9 ?>
+				<section id="single-chip" class="bg-red-grad-lt">
+					<div class="inner">
+						<div class="text-position">
+							<p class="txtw">Solo Qualcomm diseña componentes clave de tecnología que conectan cada uno de los componentes en un <span class="thick">único sistema en un chip</span></p>
+						</div>
+						<div class="graphic-position">
+							<img id="single-phone" src="img/tablet.png" />
+							<img id="pop-power" src="img/pop-power.png" />
+							<img id="pop-lines" src="img/pop-lines.png" />
+						</div>
+
+						<span class="stretch"></span>
+					</div>
+				</section>
+
+				<?php // Slide #10 ?>
+				<section id="multiple-tasks" class="bg-white">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtr">Un solo procesador. <span class="thick">Diversas tareas.</span></h2>
+							<p class="txtb">Los procesadores Snapdragon emplean distintos motores móviles, cada uno diseñado específicamente para una tarea.</p>
+						</div>
+					</div>
+          <div id="qchips">
+            <ul class="left">
+              <li class="sen"><span class="img"></span>Sensores</li>
+              <li class="dsp"><span class="img"></span>DSP</li>
+              <li class="cpu"><span class="img"></span>CPU</li>
+              <li class="gpu"><span class="img"></span>GPU</li>
+              <li class="nav"><span class="img"></span>Navigation</li>
+            </ul>
+            <ul class="mid">
+              <li class="mm onn"><span class="img"></span>Multimedia</li>
+            </ul>
+            <ul class="right">
+              <li class="pwr"><span class="img"></span>Administración del<br />consumo energético</li>
+              <li class="dis"><span class="img"></span>Pantalla</li>
+              <li class="cam"><span class="img"></span>Cámara</li>
+              <li class="sec"><span class="img"></span>Seguridad</li>
+              <li class="con"><span class="img"></span>Conectividad</li>
+            </ul>
+            <div id="qffx"></div>
+          </div>
+				</section>
+
+				<?php /*
+				<?php // Slide #11 ?>
+				<section id="cpu-performance" class="bg-black">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtr">Technologies CPU <span class="thick">performace</span> roadmap</h2>
+							<p class="txtw">Custom CPUs built from the ground up for mobile with superior performance and power efficiency</p>
+						</div>
+					</div>
+				</section>
+
+				<?php */ // Slide #12 ?>
+				<section id="gpu-performance" class="bg-grey-rdl">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtb"><span class="thick">Rendimiento</span> de gráficos mejorado</h2>
+							<h3 class="txtb"><span class="thick">Snapdragon 600</span> con un rendimiento hasta <span class="thick txtr">29 veces más rápido*</span><br />
+              <span class="thick">Snapdragon 800</span> con un rendimiento hasta <span class="thick txtr">43 veces más rápido*</span></h3>
+						</div>
+            <div class="graphic-position">
+            	<ul>
+              <li class="a200"><span class="fill"></span><span class="name">Adreno 200</span></li>
+              <li class="a205"><span class="fill"></span><span class="name">Adreno 205</span></li>
+              <li class="a220"><span class="fill"></span><span class="name">Adreno 220</span></li>
+              <li class="a225"><span class="fill"></span><span class="name">Adreno 225</span></li>
+              <li class="a320 txtw"><span class="fill"></span><span class="name">Adreno 320 / <span class="thick">Snapdragon 600</span></span></li>
+              <li class="a330 txtw"><span class="fill"></span><span class="name">Adreno 330 / <span class="thick">Snapdragon 800</span></span></li>
+              </ul>
+              <p class="center">* Que Adreno 200. Fuente: datos de QCT.</p>
+            </div>
+					</div>
+				</section>
+        
+				<?php // Slide #13 ?>
+				<section id="modem-performance" class="bg-grey-grad">
+					<div class="inner">
+						<div class="text-position center">
+							<h2 class="txtb"><span class="thick">Rendimiento</span> del módem</h2>
+						</div>
+						<div class="graphic-position center">
+							<div class="left three-floats">
+								<p>Velocidad de descarga de datos<br /><b>Hasta 150 Mbps*</b></p>
+							</div>
+							<div class="middle three-floats">
+								<img id="signal" src="img/modem-signal.png" />
+								<img id="spinning-arrows" src="img/spinning-arrows.png" />
+							</div>
+							<div class="right three-floats">
+								<p>Velocidad de carga de datos<br /><b>Hasta 50 Mbps*</b></p>
+							</div>
+
+							<span class="stretch"></span>
+              
+            	<p class="center"> *Peak measured data rates, actual speeds will vary. </p>
+
+						</div>
+					</div>
+				</section>
+
+				<?php // Slide #14 ?>
+				<section id="thermal-performance" class="bg-black">
+					<div class="inner">
+						<div class="text-position">
+							<h2 class="txtw">Mejor <span class="thick">rendimiento</span> térmico.<br />Menor consumo de energía.</h2>
+						</div>
+						<div class="graphic-position">
+							<img id="thermal-phone" src="img/butter_test.jpg" />
+						</div>
+						<div class="video-box">
+							<div class="video-button">
+								<div id="play-video" class="sprite"></div>
+								<p>Mire la prueba de la mantequilla</p>
+							</div>
+						</div>
+
+						<div id="butter-video">
+            	<iframe id="buttervid" src="//player.vimeo.com/video/77088565?title=0&amp;byline=0&amp;portrait=0&amp;color=d1252f&amp;api=1&amp;player_id=buttervid" width="960" height="540" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						</div>
+
+					</div>
+				</section>
+
+				<?php // Slide #15 ?>
+				<section id="features" class="bg-white">
+					<div class="inner">
+						<div class="graphic-position">
+							<img src="img/chip381x252.png" />
+						</div>
+						<div class="text-position">
+							<div id="connectivity">
+								<h2 class="txtr">Conectividad</h2>
+								<p class="txtb">Velocidad de descarga insuperable</p>
+							</div>
+							<div id="performance">
+								<h2 class="txtr">Rendimiento</h2>
+								<p class="txtb">Gráficos hasta 2 veces más rápidos</p>
+							</div>
+							<div id="longer-battery">
+								<h2 class="txtr">Mayor duración de la batería</h2>
+								<p class="txtb">Siempre activado, siempre conectado</p>
+							</div>
+							<div id="speed">
+								<h2 class="txtr">Velocidad</h2>
+								<p class="txtb">CPU de hasta 2,3 Ghz</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<?php // quiz CTA ?>
+        <section id="proceed" class="bg-black">
+        	<div class="inner">
+          	<div class="text-position center">
+            	<div class="video-button">
+								<h2><a href="assessment.html" class="txtw">Realice una <span class="thick">evaluación</span> <span class="sprite play"></span></a></h2>
+							</div>
+            </div>
+          </div>
+        </section>
+				<?php // Slide #16 ?>
+				<section id="legal" class="bg-red">
+					<div class="inner">
+						<div class="text-position center">
+							<p class="txtw">&copy; <?php date_default_timezone_set('UTC'); echo date('Y'); ?> Qualcomm Technologies, Inc. Todos los derechos reservados.</p>
+							<p class="txtw">Qualcomm es una marca comercial de QUALCOMM Incorporated, registrada en EE.UU. y en otros países. Gobi and Snapdragon are trademarks of Qualcomm Technologies, Inc.<br />Other products and brand names are property of their respective owners.</p>
+						</div>
+						<div class="graphic-position center">
+							<img id="logo" src="img/snapdragon-logo.png" />
+						</div>
+					</div>
+				</section>
+				
+			</div>
+
+			<footer>
+			</footer>
+	 
+		</div>
+		<script type="text/javascript" src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
+		<script src="js/animate.js"></script>
+<script type="text/javascript"> 
+//browser-update.org
+var $buoop = {vs:{i:9,f:5,o:12,s:5,n:9},reminder:0,onshow:function(infos) {
+	jQuery('#buorg > div').prepend('<strong>This module is built with the latest HTML5 + CSS3 in mind.</strong> ');
+	}}; 
+$buoop.ol = window.onload; 
+window.onload=function(){ 
+ try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
+ var e = document.createElement("script"); 
+ e.setAttribute("type", "text/javascript"); 
+ e.setAttribute("src", "//browser-update.org/update.js"); 
+ document.body.appendChild(e); 
+} 
+</script>
+	</body>
+</html>
