54e6d95d124aa34a7c4ebca9649bfc014d216158
alex chousmith <alex@ninthlink.com>
2014-02-25 14:06:54 -0800

    new wslash Family Processors training family-processors-22414-344pm https://ninthlink.teamworkpm.net/tasks/2752915

--- docroot/wtrainings/family-processors/javascripts/animation.js
+++ docroot/wtrainings/family-processors/javascripts/animation.js
@@ -1,8 +1,8 @@
 /*=========================================================================================
 Author: Tou Lee
 Created: 1/16/14
-Last Updated: 2/13/14 - 12:57pm
-Version: 1.0.0
+Last Updated: 2/24/14 - 12:59pm
+Version: 1.0.2
 
 ** This is the file for any javascript animations for the course **
 
@@ -12,27 +12,34 @@ Changelog v1.0.0
 Changelog v1.0.1
   - Added panel snapping
 
+Changelog v1.0.2
+  - Added the jquery.qualcomm-reveal.js initalization
+  - Added new animcounter variable to store the counter retrieved from the $.subscription()
+  - Added extra check certain animations (section 2, 4, 6, 7) to only trigger when it is 
+    on their slide that is showing to prevent animations for firing too early
+
 ==========================================================================================*/
 (function($) {
   /* ----------------------------------------------------------------
     Reusuable cached variables
   ---------------------------------------------------------------- */
-  var $html = $('html');
-  var isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
+  var $html = $('html'),
+      isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i),
+      animcounter;
 
   $(document).ready(function() {
     /* ----------------------------------------------------------------
-       Panel Snap
-       	- Snap to the panels when the user comes close to them
-       	- Elements with class of '.panel-snap' will snap to them
+      Qualcomm Reveal Plugin
+        - Initalize the plugin
+        - Use the counter passed from $.publish('qualcommreveal/reveal-finish')
+          to keep track of the items, and make sure animations do not
+          fire prematurely
     ---------------------------------------------------------------- */
-    $(document).scrollsnap({
-    	snaps: '.panel-snap',
-    	proximity: (isMobile) ? 200 : 350,
-    	easing: 'easeInOutExpo',
-    	duration: 200,
-    	onSnapWait: 200,
-    	onSnapEvent: 'total'
+    //Initalize the plugin
+    $('.content-container').qualcommreveal({});
+
+    $.subscribe('qualcommreveal/reveal-finish', function(event, elem, counter) { 
+      animcounter = counter; 
     });
 
     /* ----------------------------------------------------------------
@@ -41,13 +48,11 @@ Changelog v1.0.1
 		$('.s2p1 h1.light').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
 			if(isInView) {
 		   	if(visiblePartY == 'both') {
-		
-		   	//unbind the event
 		    $(this).off('inview');
+
+		    // console.log(animcounter);
+		    // console.log('SECTION 2: inview fired :: chat bubbles');
 		
-		    //console.log('SECTION 2: inview fired');
-		
-			  //var tl = new TimelineMax({onComplete: removeInView, onCompleteParams: [this]});
 			  var tl = new TimelineMax();
 			  tl.from('.speech-bubbles.cust1', 0.8, { css:{ force3D: true, autoAlpha: 0, top: '50px', left: '50%'},  ease: 'Back.easeOut'})
 		    	.from('.speech-bubbles.cust2', 0.8, { css:{ force3D: true, autoAlpha: 0, top: '50px', left: '30%'},  ease: 'Back.easeOut'})
@@ -58,16 +63,15 @@ Changelog v1.0.1
 
 
     /* ----------------------------------------------------------------
-      Section 4: Customer Chat Bubbles
+      Section 4: Technologies Abound
     ---------------------------------------------------------------- */
     $('.s4p1 .icons.pov').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
-      if(isInView) {
+      if(isInView && animcounter === 2) {
        	if(visiblePartY == 'both') {
-
-         	//unbind the event
          	$(this).off('inview');
 
-         	//console.log('SECTION 4: inview fired');
+          // console.log(animcounter);
+         	// console.log('SECTION 4: inview fired :: technology abound');
 	
          	//This is the point of convergence
          	var povOffset = $('.icons.pov').offset();
@@ -81,16 +85,15 @@ Changelog v1.0.1
   			      thisOffset = $this.offset(),
   			      relativeY = povOffset.top - thisOffset.top,
   			      relativeX = povOffset.left - thisOffset.left;
-	
+              //Tween the animation to the point of convergence
 	      			TweenMax.to($this, 0.75, { css:{ left: relativeX, top: relativeY, autoAlpha: 0 }, delay: 0.2});
-	      			//TweenMax.to($this, (i*0.15), { css:{ left: relativeX, top: relativeY, autoAlpha: 0 }, delay: 0.2});
   			 	});
 
           //Hide the container-icon
-          var tl = new TimelineMax();
+          var tl = new TimelineMax({ delay: 0.3 });
           tl.to($('.icons-container'), 1, { css:{ height: 0, autoAlpha: 0, force3D: true }, delay: 0.5 })
             .from($('.tech-phone'), 1, { css:{ autoAlpha: 0, top: '250px', force3D: true }, ease: 'Power4.easeInOut'})
-            .staggerFrom($('.tech'), 1, { css:{ autoAlpha: 0, display: 'block', rotationX: 90, force3D: true }, ease: 'Back.easeOut' }, 0.2, "-=0.75")
+            .staggerFrom($('.tech'), 1, { css:{ autoAlpha: 0, display: 'block', force3D: true }, ease: 'Back.easeOut' }, 0.2, "-=0.75")
             .staggerFrom($('.tech > p'), 0.5, { css:{ autoAlpha: 0, top: '-30px', force3D: true }}, 0.2, "-=1.1");
         }
       }
@@ -100,18 +103,17 @@ Changelog v1.0.1
     /* ----------------------------------------------------------------
       Section 6: Leadership Circles and #1
     ---------------------------------------------------------------- */
-    $('.s6p1 .square-box').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
-      if(isInView) {
+    $('.s6p1 div.square-box > h1.subheader').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+      if(isInView && animcounter === 4) {        
         if(visiblePartY == 'top' || visiblePartY == 'both') {
-
-          //unbind the event
           $(this).off('inview');
 
-          //console.log('SECTION 6: inview fired');
+          // console.log(animcounter);
+          // console.log('SECTION 6: inview fired :: #1');
 
-          var tl = new TimelineMax();
-          tl.from($(this).children('h1'), 1, { css:{ autoAlpha: 0, fontSize: '0.5em', force3D: true }, ease: 'Power4.easeInOut' })
-            .staggerFrom($('.circles'), 1.25, { css:{ autoAlpha: 0, rotationY: 180, top: '-=100px', force3D: true }, ease: 'Back.easeInOut' }, 0.2);
+          var tl = new TimelineMax({ delay: 0.3 });
+          tl.from($(this), 1, { css:{ autoAlpha: 0, fontSize: '0.5em', force3D: true }, ease: 'Power4.easeInOut' })
+            .staggerFrom($('.circles'), 1.25, { css:{ autoAlpha: 0, rotationY: 180, top: '-=80px', force3D: true }, ease: 'Back.easeInOut' }, 0.2);
         }
       }
     });
@@ -120,15 +122,13 @@ Changelog v1.0.1
     /* ----------------------------------------------------------------
       Section 7: Snapdragon Family Chips
     ---------------------------------------------------------------- */
-    // $('.s7p1 h1.subheader').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
-    $('.s7p1').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
-      if(isInView) {
+    $('.s7p1 .chip-400').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+      if(isInView && animcounter === 5) {
         if(visiblePartY == 'top' || visiblePartY == 'both') {
-
-          //unbind the event
           $(this).off('inview');
 
-          //console.log('SECTION 7: inview fired');
+          // console.log(animcounter);
+          // console.log('SECTION 7: inview fired');
 
           var tl = new TimelineMax();
           tl.staggerFrom($('.chips'), 1.2, { css:{ autoAlpha: 0, right: '-100%', force3D: true }, ease: 'Back.easeOut' }, 0.25)
@@ -143,11 +143,10 @@ Changelog v1.0.1
     $('.spec-main-img').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
       if(isInView) {
         if(visiblePartY == 'top' || visiblePartY == 'bottom' || visiblePartY == 'both') {
-
-          //unbind the event
           $(this).off('inview');
 
-          //console.log('SECTION 8,9,10,11 Spec section: inview fired');
+          // console.log(animcounter);
+          // console.log('SECTION 8,9,10,11 Spec section: inview fired');
 
           TweenMax.from($(this).find('img.spec-chip-img'), 1.5, { css:{ autoAlpha: 0, marginTop: '-200%', width: '75%', marginLeft: '-37.5%', force3D: true }, ease: 'Power4.easeInOut' });
         }
@@ -161,11 +160,10 @@ Changelog v1.0.1
     $('.cust-main-img').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
       if(isInView) {
         if(visiblePartY == 'top' || visiblePartY == 'bottom' || visiblePartY == 'both') {
-
-          //unbind the event
           $(this).off('inview');
 
-          //console.log('SECTION 8,9,10,11 Chip section: inview fired');
+          // console.log(animcounter);
+          // console.log('SECTION 8,9,10,11 Chip section: inview fired');
 
           //Tween the chip image into the spot
           TweenMax.from($(this).find('img.cust-chip-img'), 1.5, { css:{ autoAlpha: 0, marginTop: '-200%', width: '36%', marginLeft: '-18%', force3D: true }, ease: 'Power4.easeInOut' });
@@ -178,16 +176,4 @@ Changelog v1.0.1
 
 
   }); //end $(document).ready()
-
-  
-  /* ----------------------------------------------------------------
-    removeInView
-      - Removes the 'inview' event from the object
-    @params elem -- the elment have 'inview' event unbound
-  ---------------------------------------------------------------- */
-  function removeInView(elem) {
-   	$(elem).off('inview');
-    //console.log(elem + ' has "inview" event removed');
-  }
-
 })(jQuery); //end self-invoking anonymous function
