9c99cb6b176852ea7b65bcb2da57cd68acbdbf87
alex chousmith <alex@ninthlink.com>
2014-08-19 16:04:51 -0700

    replicate from MTS to hide user View Profile tab

--- docroot/sites/all/themes/elite/template.php
+++ docroot/sites/all/themes/elite/template.php
@@ -245,3 +245,32 @@ function elite_quiz_get_user_results($variables) {
   $output .= '<p><em>' . t('@quizzes that are not evaluated may have a different score and grade once evaluated.', array('@quizzes' => QUIZ_NAME)) . '</em></p>';
   return $output;
 }
+/*
+ * theme_menu_local_task
+ * to catch & remove user profile VIEW tab?
+ */
+function elite_menu_local_task($variables) {
+  $link = $variables['element']['#link'];
+  //watchdog('q catch', 'elite_menu_local_task : <pre>'. print_r($link,true) .'</pre>');
+  $link_text = $link['title'];
+
+  // catch to remove user profile VIEW tab?
+  if ( $link['path'] == 'user/%/view' ) {
+	return;
+  }
+  
+  if (!empty($variables['element']['#active'])) {
+    // Add text to indicate active tab for non-visual users.
+    $active = '<span class="element-invisible">' . t('(active tab)') . '</span>';
+
+    // If the link does not contain HTML already, check_plain() it now.
+    // After we set 'html'=TRUE the link will not be sanitized by l().
+    if (empty($link['localized_options']['html'])) {
+      $link['title'] = check_plain($link['title']);
+    }
+    $link['localized_options']['html'] = TRUE;
+    $link_text = t('!local-task-title!active', array('!local-task-title' => $link['title'], '!active' => $active));
+  }
+
+  return '<li' . (!empty($variables['element']['#active']) ? ' class="active"' : '') . '>' . l($link_text, $link['href'], $link['localized_options']) . "</li>\n";
+}
