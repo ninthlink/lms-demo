0d7fb3ec53147200dd100712cb3180022dac40b8
alex chousmith <alex@ninthlink.com>
2014-10-03 11:20:47 -0700

    add new templates for home page two bottom CTA blocks so that we can sanitize that input better there

---
+++ docroot/sites/all/themes/elite/templates/block--block--1.tpl.php
@@ -0,0 +1,15 @@
+<?php $tag = $block->subject ? 'section' : 'div'; ?>
+<<?php print $tag; ?><?php print $attributes; ?>>
+  <div class="block-inner clearfix">
+    <?php print render($title_prefix); ?>
+    <?php print render($title_suffix); ?>
+    
+    <div<?php print $content_attributes; ?>>
+	  <?php if ($block->subject): ?>
+      <h2><?php print t($block->subject); ?></h2>
+      <?php endif; ?>
+      <?php print $content ?>
+	  <div class="more-link"><a href="#"><?php print t('Coming Soon'); ?></a></div>
+    </div>
+  </div>
+</<?php print $tag; ?>>
